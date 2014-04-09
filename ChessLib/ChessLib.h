fc583b5f59b9db7f62b211caaf3eb55fa9c833f8
Anton Guryanov <guryanov91@gmail.com>
2014-04-09 11:20:01 +0400

    Initial Commit

---
+++ ChessLib/ChessLib.h
@@ -0,0 +1,13 @@
+//
+//  ChessLib.h
+//  ChessLib
+//
+//  Created by Anton Guryanov on 09.04.14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ChessLib : NSObject
+
+@end
---
+++ ChessLib/ChessLib.m
@@ -0,0 +1,13 @@
+//
+//  ChessLib.m
+//  ChessLib
+//
+//  Created by Anton Guryanov on 09.04.14.
+//
+//
+
+#import "ChessLib.h"
+
+@implementation ChessLib
+
+@end
